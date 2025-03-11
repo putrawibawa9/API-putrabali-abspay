52562ec2400c6afed9d816cc5113a198f31faf9f
I Gede Putra Wibawa <putrawibawa7@gmail.com>
2025-03-11 20:26:07 +0800

    sebelum ngide coba fitur seach dan pagination

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -36,7 +36,7 @@ Teacher::create([
 
 Teacher::create([
     'name' => 'Ahmad Fauzi',
-      'username' => 'username9',
+      'username' => 'dasd',
     'password' => bcrypt('password'),
     'alias' => 'af',
 ]);
@@ -68,6 +68,148 @@ Teacher::create([
     'password' => bcrypt('password'),
     'alias' => 'jp',
 ]);
+
+Teacher::create([
+    'name' => 'Dewi Anggraini',
+    'username' => 'username8',
+    'password' => bcrypt('password'),
+    'alias' => 'da',
+]);
+
+Teacher::create([
+    'name' => 'Budi Santoso',
+    'username' => 'yjuytru',
+    'password' => bcrypt('password'),
+    'alias' => 'bs',
+]);
+
+Teacher::create([
+    'name' => 'Siti Rohmah',
+    'username' => 'username10',
+    'password' => bcrypt('password'),
+    'alias' => 'sr',
+]);
+
+Teacher::create([
+    'name' => 'Ahmad Fauzi',
+    'username' => 'username11',
+    'password' => bcrypt('password'),
+    'alias' => 'af',
+]);
+
+Teacher::create([
+    'name' => 'Lina Kusuma',
+    'username' => 'username12',
+    'password' => bcrypt('password'),
+    'alias' => 'lk',
+]);
+
+Teacher::create([
+    'name' => 'Rudi Hartono',
+    'username' => 'username13',
+    'password' => bcrypt('password'),
+    'alias' => 'rh',
+]);
+
+Teacher::create([
+    'name' => 'Mega Putri',
+    'username' => 'username14',
+    'password' => bcrypt('password'),
+    'alias' => 'mp',
+]);
+
+Teacher::create([
+    'name' => 'Faisal Anwar',
+    'username' => 'username15',
+    'password' => bcrypt('password'),
+    'alias' => 'fa',
+]);
+
+Teacher::create([
+    'name' => 'Indah Lestari',
+    'username' => 'username16',
+    'password' => bcrypt('password'),
+    'alias' => 'il',
+]);
+
+Teacher::create([
+    'name' => 'Yusuf Hadi',
+    'username' => 'username17',
+    'password' => bcrypt('password'),
+    'alias' => 'yh',
+]);
+
+Teacher::create([
+    'name' => 'Teguh Prasetya',
+    'username' => 'username18',
+    'password' => bcrypt('password'),
+    'alias' => 'tp',
+]);
+
+Teacher::create([
+    'name' => 'Nina Kartika',
+    'username' => 'username19',
+    'password' => bcrypt('password'),
+    'alias' => 'nk',
+]);
+
+Teacher::create([
+    'name' => 'Bayu Saputra',
+    'username' => 'username20',
+    'password' => bcrypt('password'),
+    'alias' => 'bs',
+]);
+
+Teacher::create([
+    'name' => 'Rina Sari',
+    'username' => 'username21',
+    'password' => bcrypt('password'),
+    'alias' => 'rs',
+]);
+
+Teacher::create([
+    'name' => 'Dian Purnama',
+    'username' => 'username22',
+    'password' => bcrypt('password'),
+    'alias' => 'dp',
+]);
+
+Teacher::create([
+    'name' => 'Eko Setiawan',
+    'username' => 'username23',
+    'password' => bcrypt('password'),
+    'alias' => 'es',
+]);
+
+Teacher::create([
+    'name' => 'Sari Wulandari',
+    'username' => 'username24',
+    'password' => bcrypt('password'),
+    'alias' => 'sw',
+]);
+
+Teacher::create([
+    'name' => 'Gilang Ramadhan',
+    'username' => 'username25',
+    'password' => bcrypt('password'),
+    'alias' => 'gr',
+]);
+
+Teacher::create([
+    'name' => 'Andi Wijaya',
+    'username' => 'username26',
+    'password' => bcrypt('password'),
+    'alias' => 'aw',
+]);
+
+Teacher::create([
+    'name' => 'Rika Ananda',
+    'username' => 'username27',
+    'password' => bcrypt('password'),
+    'alias' => 'ra',
+]);
+
+
 
     }
 }
