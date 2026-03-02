0297e40033576363d6f7d1a6f55b73704377f260
putrawibawa9 <putrawibawa7@gmail.com>
2026-03-02 08:53:47 +0800

    done scheduler

--- routes/console.php
+++ routes/console.php
@@ -7,5 +7,7 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-Schedule::command('salary:generate-family')->everyMinute();
+Schedule::command('salary:generate-family')
+    ->dailyAt('23:55')
+    ->when(fn () => now()->day <= 26);
 Schedule::command('backup:run')->dailyAt('02:00');
